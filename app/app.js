f30d7f7b5ca84d76923a81402bd80dd8549c300b
Tyler Miller <tylermillerhome@gmail.com>
2016-11-04 15:08:34 -0700

    forgot files

--- app/app.js
+++ app/app.js
@@ -1,4 +1,34 @@
 import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
+import uiRouter from 'angular-ui-router';
 
-angular.module('my-app', [angularMaterialize])
+angular.module('my-app', [angularMaterialize, uiRouter])
+  .config(['$stateProvider', ($stateProvider) => {
+    $stateProvider
+      .state('home', {
+        url: '',
+        templateUrl: 'views/home.html',
+        // controller: 'HomeCtrl',
+        // controllerAs: 'homeCtrl',
+      })
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/home.html',
+      })
+      .state('diagonalley', {
+        url: '/diagonalley',
+        templateUrl: 'views/diagonalley.html',
+      })
+      .state('user', {
+        url: '/user',
+        templateUrl: 'views/user/user.html',
+      })
+      .state('user.profile', {
+        url: '/profile',
+        templateUrl: 'views/user/profile.html',
+      })
+      .state('user.account', {
+        url: '/account',
+        templateUrl: 'views/user/account.html',
+      });
+  }]);
